6c35b059884f15f7bee5bfa5e72f8c3a2b6edcef
DSKhrebtov <framzik@gmail.com>
2023-06-17 17:18:16 +0300

    home work3 создать подпроект в Gradle, в котором будут размещаться модели API с использованием технологии OpenAPI; создать автоматический тест для проверки корректности сериализации и десериализации моделей;

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,16 @@
 rootProject.name = "kotlin-marketplace"
 include("les1hw1")
 include("acceptence")
+include("do-yoga-api-v1-jackson")
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openapiVersion: String by settings
     plugins {
         kotlin("jvm") version kotlinVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
 
