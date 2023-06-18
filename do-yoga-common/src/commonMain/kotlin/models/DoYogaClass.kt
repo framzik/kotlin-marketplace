e0dbd720202c5ca2644ad95fd91e7d23a67b9560
DSKhrebtov <framzik@gmail.com>
2023-06-18 13:04:33 +0300

    home work3 правка замечаний

--- do-yoga-common/src/commonMain/kotlin/models/DoYogaClass.kt
+++ do-yoga-common/src/commonMain/kotlin/models/DoYogaClass.kt
@@ -5,11 +5,11 @@ import kotlinx.datetime.LocalDateTime
 
 data class DoYogaClass(
     var id: DoYogaClassId = DoYogaClassId.NONE,
-    val officeAddress: String? = null,
+    var officeAddress: String? = null,
     var visibility: DoYogaVisibility = DoYogaVisibility.NONE,
     var classType: DoYogaType = DoYogaType.NONE,
-    val trainer: String? = null,
-    val students: Set<String>? = null,
-    val time: LocalDateTime? = null,
+    var trainer: String? = null,
+    var students: Set<String>? = null,
+    var time: LocalDateTime? = null,
     val permissionsClient: MutableSet<DoYogaClassPermissionClient> = mutableSetOf()
 )
